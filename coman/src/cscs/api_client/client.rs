79d9961a8f0ef511ca900d1b1be07c18a0c1ba01
Ralf Grubenmann <ralf.grubenmann@sdsc.ethz.ch>
2025-12-10 12:57:25 +0100

    add tests

--- coman/src/cscs/api_client/client.rs
+++ coman/src/cscs/api_client/client.rs
@@ -201,3 +201,266 @@ impl CscsApi {
         Ok(result.into())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use claim::*;
+    use firecrest_client::types::{
+        DownloadFileResponse, FirecrestFilesystemTransferModelsTransferJob, GetJobMetadataResponse, GetJobResponse,
+        GetSystemsResponse, HPCCluster, JobMetadataModel, JobModel, JobStatus, PostJobSubmissionResponse,
+        S3TransferResponse, UploadFileResponse,
+    };
+    use injectorpp::interface::injector::*;
+
+    use super::*;
+
+    fn get_client() -> CscsApi {
+        CscsApi {
+            client: FirecrestClient::default(),
+        }
+    }
+
+    #[tokio::test]
+    async fn test_start_job() {
+        let client = get_client();
+        let mut injector = InjectorPP::new();
+        injector
+            .when_called_async(injectorpp::async_func!(
+                firecrest_client::compute_api::post_compute_system_job(
+                    &client.client,
+                    "",
+                    None,
+                    "",
+                    None,
+                    None,
+                    None,
+                    HashMap::new()
+                ),
+                Result<PostJobSubmissionResponse>
+            ))
+            .will_return_async(injectorpp::async_return!(
+                Ok(PostJobSubmissionResponse { job_id: Some(1) }),
+                Result<PostJobSubmissionResponse>
+            ));
+        let result = client
+            .start_job("test", None, "test", PathBuf::from("/test"), HashMap::new())
+            .await;
+        assert_ok!(result);
+    }
+
+    #[tokio::test]
+    async fn test_list_systems() {
+        {
+            let client = get_client();
+            let mut injector = InjectorPP::new();
+            injector
+                .when_called_async(injectorpp::async_func!(
+                    firecrest_client::status_api::get_status_systems(&client.client),
+                    Result<GetSystemsResponse>
+                ))
+                .will_return_async(injectorpp::async_return!(
+                    Ok(GetSystemsResponse {
+                        systems: vec![
+                            HPCCluster {
+                                name: "daint".to_owned(),
+                                ..Default::default()
+                            },
+                            HPCCluster {
+                                name: "eiger".to_owned(),
+                                ..Default::default()
+                            }
+                        ],
+                        ..Default::default()
+                    }),
+                    Result<GetSystemsResponse>
+                ));
+            let result = client.list_systems().await;
+            let systems = result.unwrap();
+            assert_eq!(systems.len(), 2);
+
+            let result = client.get_system("daint").await;
+            let system = result.unwrap();
+            assert_some!(system.clone());
+            assert_eq!(system.unwrap().name, "daint");
+        }
+    }
+
+    #[tokio::test]
+    async fn test_list_jobs() {
+        {
+            let client = get_client();
+            let mut injector = InjectorPP::new();
+            injector
+                .when_called_async(injectorpp::async_func!(
+                    firecrest_client::compute_api::get_compute_system_jobs(&client.client, "", None,),
+                    Result<GetJobResponse>
+                ))
+                .will_return_async(injectorpp::async_return!(
+                    Ok(GetJobResponse {
+                        jobs: Some(vec![
+                            JobModel {
+                                name: "Job1".to_owned(),
+                                job_id: 1,
+                                status: JobStatus {
+                                    state: "RUNNING".to_owned(),
+                                    ..Default::default()
+                                },
+                                ..Default::default()
+                            },
+                            JobModel {
+                                name: "Job2".to_owned(),
+                                job_id: 2,
+                                status: JobStatus {
+                                    state: "FAILED".to_owned(),
+                                    ..Default::default()
+                                },
+                                ..Default::default()
+                            },
+                            JobModel {
+                                name: "Job3".to_owned(),
+                                job_id: 3,
+                                status: JobStatus {
+                                    state: "COMPLETED".to_owned(),
+                                    ..Default::default()
+                                },
+                                ..Default::default()
+                            },
+                            JobModel {
+                                name: "Job4".to_owned(),
+                                job_id: 4,
+                                status: JobStatus {
+                                    state: "PENDING".to_owned(),
+                                    ..Default::default()
+                                },
+                                ..Default::default()
+                            },
+                            JobModel {
+                                name: "Job5".to_owned(),
+                                job_id: 5,
+                                status: JobStatus {
+                                    state: "CANCELLED".to_owned(),
+                                    ..Default::default()
+                                },
+                                ..Default::default()
+                            }
+                        ])
+                    }),
+                    Result<GetJobResponse>
+                ));
+            let result = client.list_jobs("daint", None).await;
+            assert_eq!(result.unwrap().len(), 5);
+        }
+    }
+
+    #[tokio::test]
+    async fn test_get_job() {
+        {
+            let client = get_client();
+            let mut injector = InjectorPP::new();
+            injector
+                .when_called_async(injectorpp::async_func!(
+                    firecrest_client::compute_api::get_compute_system_job(&client.client, "", 1,),
+                    Result<GetJobResponse>
+                ))
+                .will_return_async(injectorpp::async_return!(
+                    Ok(GetJobResponse {
+                        jobs: Some(vec![JobModel {
+                            name: "Job1".to_owned(),
+                            job_id: 1,
+                            status: JobStatus {
+                                state: "RUNNING".to_owned(),
+                                ..Default::default()
+                            },
+                            ..Default::default()
+                        }])
+                    }),
+                    Result<GetJobResponse>
+                ));
+            injector
+                .when_called_async(injectorpp::async_func!(
+                    firecrest_client::compute_api::get_compute_system_job_metadata(&client.client, "", 1,),
+                    Result<GetJobMetadataResponse>
+                ))
+                .will_return_async(injectorpp::async_return!(
+                    Ok(GetJobMetadataResponse {
+                        jobs: Some(vec![JobMetadataModel {
+                            job_id: "1".to_owned(),
+                            ..Default::default()
+                        }])
+                    }),
+                    Result<GetJobMetadataResponse>
+                ));
+            let result = client.get_job("test", 1).await;
+            assert_ok!(result);
+        }
+    }
+
+    #[tokio::test]
+    async fn test_transfer_upload() {
+        let client = get_client();
+        let mut injector = InjectorPP::new();
+        injector
+            .when_called_async(injectorpp::async_func!(
+                firecrest_client::filesystem_api::post_filesystem_transfer_upload(
+                    &client.client,
+                    "",
+                    None,
+                    PathBuf::from(""),
+                    1,
+                ),
+                Result<UploadFileResponse>
+            ))
+            .will_return_async(injectorpp::async_return!(
+                Ok(UploadFileResponse {
+                    transfer_job: FirecrestFilesystemTransferModelsTransferJob {
+                        job_id: 1,
+                        ..Default::default()
+                    },
+                    transfer_directives: DownloadFileResponseTransferDirectives::S3(S3TransferResponse {
+                        transfer_method: "s3".to_owned(),
+                        parts_upload_urls: Some(vec!["http://test/".to_owned()]),
+                        complete_upload_url: Some("http://complete".to_owned()),
+                        max_part_size: Some(10000),
+                        ..Default::default()
+                    })
+                }),
+                Result<UploadFileResponse>
+            ));
+        let result = client.transfer_upload("test", None, PathBuf::from("/test"), 100).await;
+        assert_eq!(result.unwrap().0, 1);
+    }
+
+    #[tokio::test]
+    async fn test_transfer_download() {
+        let client = get_client();
+        let mut injector = InjectorPP::new();
+        injector
+            .when_called_async(injectorpp::async_func!(
+                firecrest_client::filesystem_api::post_filesystem_transfer_download(
+                    &client.client,
+                    "",
+                    None,
+                    PathBuf::from(""),
+                ),
+                Result<DownloadFileResponse>
+            ))
+            .will_return_async(injectorpp::async_return!(
+                Ok(DownloadFileResponse {
+                    transfer_job: FirecrestFilesystemTransferModelsTransferJob {
+                        job_id: 1,
+                        ..Default::default()
+                    },
+                    transfer_directives: DownloadFileResponseTransferDirectives::S3(S3TransferResponse {
+                        transfer_method: "s3".to_owned(),
+                        download_url: Some("http://download".to_owned()),
+                        ..Default::default()
+                    })
+                }),
+                Result<DownloadFileResponse>
+            ));
+        let result = client.transfer_download("test", None, PathBuf::from("/test")).await;
+        let result = result.unwrap();
+        assert_eq!(result.0, 1);
+        assert_eq!(result.1, Url::parse("http://download").unwrap())
+    }
+}
